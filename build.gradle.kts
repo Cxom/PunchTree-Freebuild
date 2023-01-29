96fa7eb2e8f173fe7df9c68bda0cfb5321e4dc2b
Cxom <cxomtdoh@yahoo.com>
2023-01-29 00:28:18 -0500

    Add buildAndPublish task to build.gradle that uploads over ftp

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,17 +12,48 @@ repositories {
     maven { url = uri("https://maven.enginehub.org/repo/") }
 }
 
+val ftpAntTask by configurations.creating
+
 dependencies {
     paperDevBundle("1.19.3-R0.1-SNAPSHOT")
     paperweightDevelopmentBundle("io.papermc.paper:dev-bundle:1.19.3-R0.1-SNAPSHOT")
 
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.1.0-SNAPSHOT")
+
+    ftpAntTask("org.apache.ant:ant-commons-net:1.10.12") {
+        module("commons-net:commons-net:1.4.1") {
+            dependencies("oro:oro:2.0.8:jar")
+        }
+    }
 }
 
 tasks {
     build {
         dependsOn(reobfJar)
     }
+}
+
+val ftpHostUrl: String by project
+val ftpUsername: String by project
+val ftpPassword: String by project
+
+task("uploadToServer") {
+    doLast{
+        ant.withGroovyBuilder {
+            "taskdef"("name" to "ftp", "classname" to "org.apache.tools.ant.taskdefs.optional.net.FTP", "classpath" to ftpAntTask.asPath)
+            "ftp"("server" to ftpHostUrl, "userid" to ftpUsername, "password" to ftpPassword, "remoteDir" to "/default/plugins") {
+                "fileset"("dir" to "build/libs") {
+                    "include"("name" to rootProject.name + "-" + version + ".jar")
+                }
+            }
+        }
+    }
+}
+
+task("buildAndPublish") {
+    dependsOn("build")
+    dependsOn("uploadToServer")
+    tasks.findByName("uploadToServer")!!.mustRunAfter("build")
 }
 
 java {
