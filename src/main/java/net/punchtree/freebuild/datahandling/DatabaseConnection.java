406eac73ad1e04efe431933b9ee472141f98baeb
DevSock <51889683+DevSock@users.noreply.github.com>
2023-03-16 10:53:15 -0400

    Add DatabaseConnection

---
+++ src/main/java/net/punchtree/freebuild/datahandling/DatabaseConnection.java
@@ -0,0 +1,16 @@
+package net.punchtree.freebuild.datahandling;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+public interface DatabaseConnection {
+    CompletableFuture<? extends DatabaseConnection> connect(boolean forced);
+
+    void disconnect();
+    Map<String, Object> create(String table, Map<String, Object> values) throws Exception;
+    Optional<Map<String, Object>> read(String table);
+    Map<String, Object> update(String table, Map<String, Object> values) throws Exception;
+    Map<String, Object> delete(String table) throws Exception;
+
+}
