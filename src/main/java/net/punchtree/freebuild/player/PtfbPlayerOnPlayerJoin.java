df83218c2559e460657b80022c68e0988aca112e
DevSock <51889683+DevSock@users.noreply.github.com>
2023-03-20 23:36:18 -0400

    Implement new ptfbplayer wrapper methods

--- src/main/java/net/punchtree/freebuild/player/PtfbPlayerOnPlayerJoin.java
+++ src/main/java/net/punchtree/freebuild/player/PtfbPlayerOnPlayerJoin.java
@@ -1,22 +1,19 @@
 package net.punchtree.freebuild.player;
 
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PtfbPlayerOnPlayerJoin implements Listener {
-    private static final PlainTextComponentSerializer serializer = PlainTextComponentSerializer.plainText();
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         PtfbPlayer ptfbPlayer = PtfbPlayer.get(event.getPlayer());
-        PlayerDataHandler playerDataHandler = ptfbPlayer.getPlayerDataHandler();
 
-        playerDataHandler.withDataLoaded(handler -> {
-            if(handler.getPlayerData("", "uuid").isPresent()) return;
-            handler.setPlayerData("", "uuid", ptfbPlayer.getUuid().toString());
-            handler.saveData();
+        ptfbPlayer.withDataLoaded(loadedPtfbPlayer -> {
+            if(loadedPtfbPlayer.getPlayerData("", "uuid").isPresent()) return;
+            loadedPtfbPlayer.setPlayerData("", "uuid", ptfbPlayer.getUuid().toString());
+            loadedPtfbPlayer.saveData();
         });
     }
 }
